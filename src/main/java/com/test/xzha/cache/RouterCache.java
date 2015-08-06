0e6399ac912cdb66acfbc21b7f4ee0f8c8c17f1e
Puzikov Dmitry <puzikov@1mps.ru>
2015-08-06 18:01:03 +0300

    Fix related to cache usage.

--- src/main/java/com/test/xzha/cache/RouterCache.java
+++ src/main/java/com/test/xzha/cache/RouterCache.java
@@ -11,19 +11,19 @@ import org.apache.log4j.Logger;
  */
 public class RouterCache {
 	private static final Logger LOG = Logger.getLogger(RouterCache.class);
-	private static CacheManager CACHE_MANAGER = CacheManager.create();
+	private static CacheManager CACHE_MANAGER = CacheManager.getInstance();
 	private Cache memCache;
 
 	/**
 	 * RouterCache constructor
 	 * @param cacheName String cache name
-	 * @param capacity int max number of elements in cache
-	 * @param ttl long time to live, sec
-	 * @param tti long time to idle, sec
 	 */
-	public RouterCache (String cacheName, int capacity, long ttl, long tti) {
-		memCache = new Cache(cacheName, capacity, false, false, ttl, tti);
-		CACHE_MANAGER.addCache(memCache);
+	public RouterCache (String cacheName) {
+		memCache = CACHE_MANAGER.getCache(cacheName);
+		if (memCache == null) {
+			CACHE_MANAGER.addCache(cacheName);
+			memCache = CACHE_MANAGER.getCache(cacheName);
+		}
 	}
 
 	/**
